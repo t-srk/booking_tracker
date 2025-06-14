b5a75b7a3e4e9caedf34d1fa01234dd5ca95170d
t-srk <82791909+t-srk@users.noreply.github.com>
2025-06-13 21:14:28 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BookingTracker from "./BookingTracker";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Booking Tracker</h1>
+      <BookingTracker />
+    </div>
+  );
+}
+
+export default App;
